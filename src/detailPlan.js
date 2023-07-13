bd91bce267cfc5cacc6b55ca672ea624cd86e1e5
leaftha <vltpcks@gmail.com>
2023-07-13 10:48:55 +0900

    detail num

---
+++ src/detailPlan.js
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+
+const DetailPlan = ({ salary, name, num }) => {
+    const [detailNum, setDetailNum] = useState(num);
+    const [detailPlan, setDetailPlan] = useState([]);
+
+    const submit = (e) => {
+        e.preventDefault();
+        let name = e.target.name.value;
+        let account = e.target.num.value;
+        setDetailPlan([...detailPlan, { name: name, num: account }]);
+        setDetailNum(detailNum - account);
+    };
+    return (
+        <div>
+            <h1>{name}</h1>
+            <h2>{(num * salary) / 10}%</h2>
+            <span>값 - {num}</span>
+            <span>계산 값 - {detailNum}</span>
+            <form onSubmit={submit}>
+                <input name="name" type="text" />
+                <input name="num" type="number" />
+                <button>추가</button>
+            </form>
+            <ul>
+                {detailPlan.map((plan, idx) => {
+                    return <li key={idx}>{`${plan.name} - ${plan.num}`}</li>;
+                })}
+            </ul>
+        </div>
+    );
+};
+
+export default DetailPlan;
--- src/setplan.js
+++ src/setplan.js
@@ -1,27 +1,33 @@
 import { useState } from 'react';
+import DetailPlan from './detailPlan';
 
 const SetPlan = ({ salary }) => {
-    const [plan, setPlan] = useState([]);
+    const [plans, setPlans] = useState([]);
 
     const Submit = (e) => {
         e.preventDefault();
         let name = e.target.name.value;
-        let account = (salary * e.target.parsent.value) / 100;
+        let account = (salary * e.target.parsent.value) / 10;
         let addPlan = {
             name: name,
             num: account,
         };
-        setPlan([...plan, addPlan]);
+        setPlans([...plans, addPlan]);
     };
 
     return (
-        <form onSubmit={Submit}>
-            <label>용도</label>
-            <input name="name" type="text" />
-            <label>비율</label>
-            <input name="parsent" type="number" min={0} max={100} />
-            <button>추가</button>
-        </form>
+        <div>
+            <form onSubmit={Submit}>
+                <label>용도</label>
+                <input name="name" type="text" />
+                <label>비율</label>
+                <input name="parsent" type="number" min={0} max={100} />
+                <button>추가</button>
+            </form>
+            {plans.map((plan, idx) => {
+                return <DetailPlan key={idx} salary={salary} name={plan.name} num={plan.num} />;
+            })}
+        </div>
     );
 };
 
